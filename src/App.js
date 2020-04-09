021eaa9be5d7bad0a9042c59354c848166f8c6a9
spazdrum <spazdrum@gmail.com>
2020-04-08 22:08:52 -0400

    completed step 2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,26 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your application's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
+  const [homeScore, setScore] = useState(0);
+  
+  const homeTD = e => {
+    setScore(homeScore + 7)
+  }
+
+  const homeFG = e => {
+    setScore(homeScore + 3)
+  }
+
+  let [awayScore, scoreSet] = useState(0);
+  
+  const awayTD = e => {
+    scoreSet(awayScore + 7)
+  }
+  
+  const awayFG = e => {
+    scoreSet(awayScore + 3)
+  }
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -16,12 +36,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hard-coded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score"></div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score"></div>
           </div>
         </div>
         <BottomRow />
